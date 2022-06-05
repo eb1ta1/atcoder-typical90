3692e02228e9099467c63c636c536d7d135d938d
eb1ta1 <ebitaiume@gmail.ccom>
2022-06-05 22:44:20 +0900

    add 010

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,16 +1,16 @@
 #![allow(unused)]
 use proconio::{fastout, input};
- 
+
 fn main() {
     input! {
         h: usize,
         w: usize,
         a: [[usize; w]; h],
     }
- 
+
     let rsum: Vec<usize> = (0..h).map(|i| a[i].iter().sum()).collect();
     let csum: Vec<usize> = (0..w).map(|j| a.iter().map(|r| r[j]).sum()).collect();
- 
+
     for i in 0..h {
         for j in 0..w {
             let t = rsum[i] + csum[j] - a[i][j];
--- typical90/src/bin/010.rs
+++ typical90/src/bin/010.rs
@@ -1,6 +1,24 @@
-use proconio::{fastout, input};
+use proconio::input;
 
-#[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        cp: [(usize, i32); n],
+        q: usize,
+        lr: [(usize, usize); q]
+    }
+    let mut first_sum = vec![0];
+    let mut second_sum = vec![0];
+    for i in 1..n+1 {
+        if cp[i - 1].0 == 1 {
+            first_sum.push(first_sum[i - 1] + cp[i - 1].1);
+            second_sum.push(second_sum[i - 1]);
+        } else {
+            first_sum.push(first_sum[i - 1]);
+            second_sum.push(second_sum[i - 1] + cp[i - 1].1);
+        }
+    }
+    for (l, r) in lr {
+        println!("{} {}", first_sum[r] - first_sum[l - 1], second_sum[r] - second_sum[l - 1]);
+    }
 }
