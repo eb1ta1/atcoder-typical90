000a740cf1338fca352c89898686c06c8fc161e7
eb1ta1 <ebitaiume@gmail.ccom>
2022-06-05 21:52:07 +0900

    add 004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,6 +1,23 @@
+#![allow(unused)]
 use proconio::{fastout, input};
-
-#[fastout]
+ 
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        a: [[usize; w]; h],
+    }
+ 
+    let rsum: Vec<usize> = (0..h).map(|i| a[i].iter().sum()).collect();
+    let csum: Vec<usize> = (0..w).map(|j| a.iter().map(|r| r[j]).sum()).collect();
+ 
+    for i in 0..h {
+        for j in 0..w {
+            let t = rsum[i] + csum[j] - a[i][j];
+            
+            if i != h || j != w {print!("{} ", t);}
+            else {print!("{}", t)}
+        }
+        println!();
+    }
 }
