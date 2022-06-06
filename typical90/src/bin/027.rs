c5540efa6f61380369d00afcfc3c42d41191acf1
eb1ta1 <ebitaiume@gmail.ccom>
2022-06-06 21:33:16 +0900

    add 027

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,6 +1,18 @@
+use proconio::marker::Chars;
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        ordered_user_names: [Chars; n]
+    }
+    let mut exist_user_names = HashSet::new();
+    for i in 1..n + 1 {
+        if !exist_user_names.contains(&ordered_user_names[i - 1]) {
+            println!("{}", i);
+            exist_user_names.insert(&ordered_user_names[i - 1]);
+        }
+    }
 }
