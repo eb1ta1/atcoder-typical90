cd7c167fff4a3fd008925b8e624843afaaae59ba
eb1ta1 <ebitaiume@gmail.ccom>
2022-06-06 22:15:32 +0900

    add 033

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -1,6 +1,29 @@
+use num_integer::div_floor;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize
+    }
+    if h == 1 || w == 1 {
+        println!("{}", h * w)
+    } else {
+        let led_h;
+        let led_w;
+        if h % 2 == 0 {
+            led_h = h / 2;
+        } else {
+            led_h = div_floor(h, 2) + 1;
+        }
+
+        if w % 2 == 0 {
+            led_w = w / 2;
+        } else {
+            led_w = div_floor(w, 2) + 1;
+        }
+
+        println!("{}", led_h * led_w)
+    }
 }
