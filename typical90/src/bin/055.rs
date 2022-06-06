d773faf0b05f686f8d119a60572e0e1da935c3a0
eb1ta1 <ebitaiume@gmail.ccom>
2022-06-06 23:33:59 +0900

    add 055

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -1,6 +1,26 @@
 use proconio::{fastout, input};
-
 #[fastout]
+//TODO 再帰関数を使った再実装を行う
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        p: usize,
+        q: usize,
+        a: [usize; n]
+    }
+    let mut cnt = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                for l in k + 1..n {
+                    for m in l + 1..n {
+                        if a[i] % p * a[j] % p * a[k] % p * a[l] % p * a[m] % p == q {
+                            cnt += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", cnt)
 }
