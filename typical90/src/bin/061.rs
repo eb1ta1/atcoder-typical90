3800dec174d3724294a8baf543bc3fc980ce23a3
eb1ta1 <ebitaiume@gmail.ccom>
2022-06-07 21:29:51 +0900

    add 061

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -1,6 +1,23 @@
-use proconio::{fastout, input};
+use proconio::input;
+use std::collections::VecDeque;
 
-#[fastout]
 fn main() {
-    input! {}
+    input! {
+        q: usize,
+        tx: [(usize, usize);q]
+    }
+    let mut cards = VecDeque::new();
+    for unique_tx in tx {
+        let t = unique_tx.0;
+        let x = unique_tx.1;
+        if t == 1 {
+            cards.push_front(x)
+        } else if t == 2 {
+            cards.push_back(x)
+        } else if t == 3 {
+            println!("{}", cards[x-1])
+        } else {
+            println!("Something went wrong")
+        }
+    }
 }
